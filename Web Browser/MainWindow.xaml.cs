e2f76b2fe88ff914c26aa008c82a13e705fa5765
Kevin Chang <kckaiwei@gmail.com>
2016-12-01 11:04:21 -0600

    url in textbox updates when loaded, regex for URI parsing.

--- Web Browser/MainWindow.xaml.cs
+++ Web Browser/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -35,7 +36,21 @@ namespace Web_Browser
         private void goButton_Click(object sender, RoutedEventArgs e)
         {
             string page = searchBar.Text.Trim();
+            page = Regex.Replace(page, "https://", "").Trim();
+            page = Regex.Replace(page, "http://", "").Trim();
+            page = "https://" + page;
             webBrowser.Navigate(page);
+            
+        }
+
+        private void searchBar_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void webBrowser_LoadCompleted(object sender, NavigationEventArgs e)
+        {
+            searchBar.Text = webBrowser.Source.AbsoluteUri;
         }
     }
 }
